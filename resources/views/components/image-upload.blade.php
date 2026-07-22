ba00bd8a96058d741f124fe37f3f2ffa102b8add
wsydn <wsydney@web.de>
2026-07-22 22:43:30 +0200

    Refactor image upload component for improved file handling and UI consistency

--- resources/views/components/image-upload.blade.php
+++ resources/views/components/image-upload.blade.php
@@ -24,40 +24,40 @@
     <flux:label>{{ $label }}</flux:label>
 @endif
 
-<flux:card class="mt-2">
+<flux:card size="sm" class="mt-2">
     <div class="flex items-start gap-8">
-        {{-- Current image preview --}}
-        @if ($hasMedia)
-            <div>
-                <img
-                    src="{{ $model->getFirstMediaUrl($collection, $thumbConversion) }}"
-                    alt="{{ __('Current image') }}"
-                    class="h-32 w-auto rounded-md object-cover"
-                />
-            </div>
-        @endif
-
         {{-- File picker --}}
         <div>
-            <flux:input :id="$name" :name="$name" type="file" :accept="$accept" />
+            <flux:input
+                :label="$hasMedia ? __('Replace image') : __('Upload image')"
+                :id="$name"
+                :name="$name"
+                type="file"
+                :accept="$accept"
+            />
             @error($name)
                 <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
             @enderror
         </div>
-
-        {{-- Delete checkbox (only when an image already exists) --}}
-        @if ($hasMedia)
-            <div class="mt-2">
-                <flux:checkbox
-                    :label="__('Delete current image')"
-                    :name="$deleteName"
-                    value="1"
-                    :checked="(bool) $deleteOld"
-                />
-                <p class="mt-2 text-xs text-zinc-400">
-                    {{ __('Or upload a new image to replace it.') }}
-                </p>
-            </div>
-        @endif
     </div>
+
+    @if ($hasMedia)
+        {{-- Delete checkbox (only when an image already exists) --}}
+        <flux:file-item
+            class="mt-4"
+            :heading="$model->getFirstMedia($collection)?->file_name"
+            :image="$model->getFirstMediaUrl($collection, $thumbConversion)"
+        >
+            <x-slot name="actions">
+                <div class="pt-1 pr-2">
+                    <flux:checkbox
+                        :label="__('Delete')"
+                        :name="$deleteName"
+                        value="1"
+                        :checked="(bool) $deleteOld"
+                    />
+                </div>
+            </x-slot>
+        </flux:file-item>
+    @endif
 </flux:card>
